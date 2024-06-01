73031aa9720654e3a7fdaa2c11ea761654b5f16b
Nikita <nik.conder@ya.ru>
2024-06-01 08:43:41 +0300

    feat: blur balance

--- lib/presentation/ui/home/home_page.dart
+++ lib/presentation/ui/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:fin_control/config.dart';
 import 'package:fin_control/data/models/profile.dart';
 import 'package:fin_control/data/models/transaction.dart';
@@ -111,10 +113,19 @@ class _HomePageState extends State<HomePage> {
                             profile.name,
                             style: textTheme.headlineMedium,
                           ),
-                          Text(
-                            "${Utils.getFormattedCurrencyToSymbol(profile.currency)} ${profile.balance}",
-                            style: textTheme.titleLarge,
-                          ),
+                          Tooltip(
+                            message: "Your current balance (Hidden)",
+                            child: ImageFiltered(
+                              imageFilter: ImageFilter.blur(
+                                sigmaX: 6.0,
+                                sigmaY: 4.0,
+                              ),
+                              child: Text(
+                                "${Utils.getFormattedCurrencyToSymbol(profile.currency)} ${profile.balance}",
+                                style: textTheme.titleLarge,
+                              ),
+                            ),
+                          )
                         ],
                       ),
                     ),
@@ -139,14 +150,22 @@ class _HomePageState extends State<HomePage> {
                             clipBehavior: Clip.antiAlias,
                             borderRadius: BorderRadius.circular(20),
                             child: Container(
-                              padding: const EdgeInsets.only(
-                                  left: 8, top: 4, right: 8, bottom: 4),
-                              color: colorScheme.background,
-                              child: Text(
-                                "${Utils.getFormattedCurrencyToSymbol(profile.currency)} ${profile.balance}",
-                                style: textTheme.titleSmall,
-                              ),
-                            ))
+                                padding: const EdgeInsets.only(
+                                    left: 8, top: 4, right: 8, bottom: 4),
+                                color: colorScheme.background,
+                                child: Tooltip(
+                                  message: "Your current balance (Hidden)",
+                                  child: ImageFiltered(
+                                    imageFilter: ImageFilter.blur(
+                                      sigmaX: 6.0,
+                                      sigmaY: 4.0,
+                                    ),
+                                    child: Text(
+                                      "${Utils.getFormattedCurrencyToSymbol(profile.currency)} ${profile.balance}",
+                                      style: textTheme.titleSmall,
+                                    ),
+                                  ),
+                                )))
                       ],
                     ),
                   ));
